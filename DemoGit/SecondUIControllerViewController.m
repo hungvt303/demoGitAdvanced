8e6e7dcc1877e27a5701a4d5b690750d21292c03
iOS18 <ios18@Dato-2.local>
2013-09-30 20:51:22 +0700

    bo sung second view controller

---
+++ DemoGit/SecondUIControllerViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecondUIControllerViewController.m
+//  DemoGit
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import "SecondUIControllerViewController.h"
+
+@interface SecondUIControllerViewController ()
+
+@end
+
+@implementation SecondUIControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
